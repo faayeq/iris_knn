46a6c743f526bb35aa775cdbb37764dbbe052bc9
faayeq <muhammadfaayeq@gmail.com>
2026-02-22 23:46:27 +0500

    feat: train test split

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import random
 
 dataset = load_iris()
 samples = [10,25,49,50,51,100,101]
-SAMPLE_NUM = 15
+SAMPLE_NUM = 150
 features_X=dataset.data
 trgt_labels_y=dataset.target
-
+X_train = []
+X_test = []
+y_train = []
+y_test =[]
 
 def euclidean_distance(p, q):
 
@@ -24,9 +27,9 @@ def euclidean_distance(p, q):
 
 def shuffle_features_and_labels():
 
-    fts_and_lbls = [features_X[0], trgt_labels_y[0]]
-    for i in range(1, SAMPLE_NUM):
-        fts_and_lbls .append( [features_X[i], trgt_labels_y[i]])
+    fts_and_lbls = []
+    for i in range(0, SAMPLE_NUM):
+        fts_and_lbls .append([ features_X[i], trgt_labels_y[i] ])
 
     random.shuffle(fts_and_lbls)
 
@@ -36,13 +39,8 @@ def shuffle_features_and_labels():
         features_X[i], trgt_labels_y[i] = fts_and_lbls[i]
         
 
-        
-
-
-
-
-def main():
-    print(  "Hello from wine-knn!")
+def print_details():
+    print(  "Hello from iris_knn!")
     print(dataset.target[samples])  # format to print the class of a sample (0,1,2) 
     print(dataset.target_names[dataset.target[samples]]) # to print the names of the samples' classes
     print(dataset.data[samples]) #this is for feature value, not for feature name
@@ -52,16 +50,44 @@ def main():
     pminusq=euclidean_distance(p,q)
     print(pminusq)
 
-    random.shuffle(dataset.data)
     print(dataset.target[samples])  # format to print the class of a sample (0,1,2) 
     print(dataset.target_names[dataset.target[samples]]) # to print the names of the samples' classes
     print(dataset.feature_names[0])
     print(dataset.feature_names[1])
     print(dataset.feature_names[2])
     print(dataset.feature_names[3])
+        
 
-    
 
+
+def train_test_split():
+
+    train_ratio = 80
+    test_ratio = 20 
+
+    training_samples_num = int((train_ratio * SAMPLE_NUM) / 100)
+    test_samples_num =(test_ratio * SAMPLE_NUM) / 100
+
+    for i in range(0, training_samples_num):
+        X_train.append(features_X[i])
+        y_train.append(trgt_labels_y[i])
+
+    for i in range(training_samples_num, SAMPLE_NUM):
+        X_test.append(features_X[i])
+        y_test.append(trgt_labels_y[i])
+
+        
+    print("verifying split: \n")
+    print(f"total test samples: {len(y_test)}")
+    for i in range(len(dataset.target_names)):
+        count = y_test.count(i)
+        print(f"class {i} ({dataset.target_names[i]}): {count} samples")
+
+
+def main():
+    shuffle_features_and_labels()
+    print_details()
+    train_test_split()
 
 
 if __name__ == "__main__":
